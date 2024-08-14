ec54b73306d905e3d72953d3e4493b7cf91cce08
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-08-14 10:45:34 +0530

    fetchCrops details

--- src/components/Crop.js
+++ src/components/Crop.js
@@ -1,73 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import FarmerDetails from "./FarmerDetails";
 
 const Crop = () => {
-	const { farmerId } = useParams();
-	const [farmer, setFarmer] = useState({});
-	const [error, setError] = useState(null);
-	const [loading, setLoading] = useState(false);
+  const { farmerId } = useParams();
+  const [farmer, setFarmer] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [crops, setCrops] = useState([]);
 
-	useEffect(() => {
-		fetchFarmerDetails();
-	}, [farmerId])
+  useEffect(() => {
+    fetchFarmerDetails();
+    fetchCrops();
+  }, [farmerId]);
 
-	const fetchFarmerDetails = async () => {
-		setLoading(true);
-		try {
-			const response = await fetch(
-				`http://localhost:6001/api/farmer/${farmerId}/get-farmer`,
+  const fetchFarmerDetails = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `http://localhost:6001/api/farmer/${farmerId}/get-farmer`,
         {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
           },
         }
-			);
-			const data = await response.json();
-			setFarmer(data);
-			console.log(farmer);
-			
-			setLoading(false);
-		} catch (error) {
-			setError(error);
-			setLoading(false);
-		}
-	}
+      );
+      const data = await response.json();
+      setFarmer(data);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
 
-	if(error) return <div>Error..!!</div>
-	if(loading) return <div>Loading..!!</div>
+  const fetchCrops = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `http://localhost:6001/api/crop/${farmerId}/crops`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      setCrops(data);
+      console.log(data);
+
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  if (error) return <div>Error..!!</div>;
+  if (loading) return <div>Loading..!!</div>;
 
   return (
     <div className="mx-auto p-6 bg-white">
-			<h1 className="text-4xl font-bold text-gray-900 text-center pb-4">Farmer's Details</h1>
-      <div className="mx-auto max-w-5xl px-8 py-6 flex flex-row space-x-4 justify-between items-center shadow-lg rounded-lg overflow-hidden bg-gradient-to-r from-green-200 to-blue-200 hover:from-green-400 hover:to-blue-400">
-				<div className="flex items-center gap-4">
-					<div className="relative w-16 h-16 overflow-hidden bg-gray-200 rounded-full border-4 border-green-500">
-						<svg className="absolute w-16 h-16 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-							<path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path>
-						</svg>
-					</div>
-					<div className="font-semibold">
-						<div className="text-lg text-gray-900">{farmer.name}</div>
-						<div className="text-sm text-gray-600">{farmer.email}</div>
-        	</div>
-				</div>
-        <div className="text-gray-700">
-					<p className="font-semibold text-center text-gray-800">Address</p>
-					<p className="text-center">{farmer.address}</p>
-				</div>
-				<div className="text-gray-700">
-					<p className="font-semibold text-center text-gray-800">Contact Number</p>
-					<p className="text-center">{farmer.contactNumber}</p>
-				</div>
-				<div className="text-gray-700">
-					<p className="font-semibold text-center text-gray-800">Farm Type</p>
-					<p className="text-center">{farmer.farm_type}</p>
-				</div>
-      </div>
+      <h1 className="text-4xl font-bold text-gray-900 text-center pb-4">
+        Farmer's Details
+      </h1>
+      <FarmerDetails farmer={farmer} />
     </div>
   );
-}
-
+};
 
 export default Crop;
