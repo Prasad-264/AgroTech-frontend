c2275acbad2f58a76260ecd8867b728040e70279
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-08-15 21:40:09 +0530

    called pesticide comp

--- src/components/Chemical.js
+++ src/components/Chemical.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import CropDetails from './CropDetails';
 import Fertilizer from './Fertilizer';
+import Pesticide from './Pesticide';
 
 const Chemical = () => {
   const { cropId } = useParams();
   
   return (
-    <div className="mx-auto px-6 bg-white">
+    <div className="mx-auto px-6 pb-6 bg-white">
       <h1 className="text-4xl font-bold text-gray-900 text-center m-4">
         Crop Details
       </h1>
@@ -16,6 +17,7 @@ const Chemical = () => {
         Manage fertilizers and pesticides for this crop
       </h1>
       <Fertilizer cropId={cropId} />
+      <Pesticide cropId={cropId} />
     </div>
   )
 }
